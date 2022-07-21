91612d6f1ae549c9e5d43c4c95b065e5721edb74
GabrielWarner <gabe0412@uw.edu>
2022-07-21 10:02:15 -0700

    added write file

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const generateHtml = require('./util/generateHtml')
+const Employee = require('./lib/Employee')
+const generate = require("./util/generateHtml")
+const fs = require('fs')
 
+const team = []
 const engineers = []
 const interns = []
 
@@ -31,6 +35,7 @@ const start = () => {
         },
     ]).then(ans=>{
         const manager = new Manager(ans.managerName, ans.managerId, ans.managerEmail, ans.officeNumber)
+        team.push(manager)
         askQuestion();
     })
 }
@@ -54,7 +59,7 @@ const askQuestion = () =>{
                 break;
 
             case "Finish Building Team":
-                generateHtml();
+                createHtml();
                 break;
 
             default:
@@ -64,3 +69,68 @@ const askQuestion = () =>{
     })
 }
 
+const createEngineer = () => {
+    inquirer.prompt([
+        {
+        type: 'input',
+        message: 'Enter Engineers name: ',
+        name: 'engineerName'
+        },
+        {
+            type: 'input',
+            message: 'Enter Engineers ID: ',
+            name: 'engineerId'
+        },
+        {
+            type: 'input',
+            message: 'Enter Engineers email: ',
+            name: 'engineerEmail'
+        },
+        {
+        type: 'input',
+        message: 'Enter github: ',
+        name: 'engineerGithub'
+        },
+    ]).then(ans=>{
+        const engineer = new Engineer(ans.engineerName, ans.engineerId, ans.engineerEmail, ans.engineerGithub)
+        team.push(engineer)
+        askQuestion();
+    })
+}
+
+const createIntern = () => {
+    inquirer.prompt([
+        {
+        type: 'input',
+        message: 'Enter Interns name: ',
+        name: 'internName'
+        },
+        {
+            type: 'input',
+            message: 'Enter Interns ID: ',
+            name: 'internId'
+        },
+        {
+            type: 'input',
+            message: 'Enter Interns email: ',
+            name: 'internEmail'
+        },
+        {
+        type: 'input',
+        message: 'Enter github: ',
+        name: 'internGithub'
+        },
+    ]).then(ans=>{
+        const intern = new Intern(ans.internName, ans.internId, ans.internEmail, ans.internGithub)
+        team.push(intern)
+        askQuestion();
+    })
+}
+
+const createHtml = () => {
+    const HTML = generate(team)
+    fs.writeFileSync('test.html', HTML);
+    
+}
+
+start()
